eae1942e36d459469167b5deb14b98b951c00844
Axelbresa <bresanovichaxel43@gmail.com>
2023-08-16 12:40:59 -0300

    se agregaron las validaciones en el app.js y se arreglo el error al momento de hacer click

--- validacion prueba/app.js
+++ validacion prueba/app.js
@@ -11,6 +11,35 @@ app.get("/", (req, res)=>{
     res.render("index")
 })
 
+app.post("/registrar", [
+   body("nya", "ingrese un nombre y apellido completo")
+    .exists()
+    .isLength({min:5}),
+   body("correo", "ingrese un email valido")
+    .exists()
+    .isEmail(),
+   body("edad", "ingrese un valor numerico")
+    .exists()
+    .isNumeric()
+], (req, res)=>{
+    // const errors=validationResult(req);
+    // if(!errors.isEmpty()){
+    //     res.status(400).json({errors: errors.array()});
+    //     console.log(errors)
+    // }
+
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+        console.log(req.body)
+        const valores=req.body
+        const validaciones=errors.array()
+        res.render("index", {validaciones:validaciones, valores:valores})
+    }else{
+        res.send("validacion exitosa")
+    }
+})
+
+
 app.listen(3000, ()=>{
     console.log("el server esta escuchando")
 })
